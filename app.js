70bc51c3182ecae66831c216597ec5faf66284f6
Michael B <michael@sablecliff.com>
2015-07-31 23:54:08 +1000

    Merged dev's comments into master and followed the js-standard linter's requests for pretty code

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 /**
- *		Author: Michael
- *		1 Sentence Diary implementation using node and telegram
+ *    Author: Michael
+ *    1 Sentence Diary implementation using node and telegram
  *
 */
 
@@ -15,138 +15,137 @@ var statics = require('./statics')
 
 var db = mongo(mongoURI, ['entries', 'users'])
 
-/**********************************************************************
-			Commands
+/* *********************************************************************
+      Commands
 */
 
 var keywords = {
-	'<help>': function(msg) {
-		var str = 'Send your diary sentences to this account and we will store them for you! '+
-							'Other commands include: <removeAllData>, <generateview>, <stats> and <date>. '+
-							'In order to see what they do send both <help> and a command in a message. For '+
-							'example: "<help> <generateview>" would give help about the <generateview> command.'
-		tg.send(msg.from.print_name, str)
-	}
-, '<removealldata>':function(msg) {
-	var str = 'NOT CURRENTLY IMPLEMENTED. '
-	tg.send(msg.from.print_name, str)
-}
-, '<generateview>':function(msg) {
-	var str = 'NOT CURRENTLY IMPLEMENTED. '
-	tg.send(msg.from.print_name, str)
-}
-, '<stats>':function(msg) {
-	var str = 'NOT CURRENTLY IMPLEMENTED. '
-	tg.send(msg.from.print_name, str)
-}
-, '<date>':function(msg) {
-	var str = 'NOT CURRENTLY IMPLEMENTED. '
-	tg.send(msg.from.print_name, str)
-}}
+  '<help>': function (msg) {
+    var str = 'Send your diary sentences to this account and we will store them for you! ' +
+              'Other commands include: <removeAllData>, <generateview>, <stats> and <date>. ' +
+              'In order to see what they do send both <help> and a command in a message. For ' +
+              'example: "<help> <generateview>" would give help about the <generateview> command.'
+    tg.send(msg.from.print_name, str)
+  },
+  '<removealldata>': function (msg) {
+    var str = 'NOT CURRENTLY IMPLEMENTED. '
+    tg.send(msg.from.print_name, str)
+  },
+  '<generateview>': function (msg) {
+    var str = 'NOT CURRENTLY IMPLEMENTED. '
+    tg.send(msg.from.print_name, str)
+  },
+  '<stats>': function (msg) {
+    var str = 'NOT CURRENTLY IMPLEMENTED. '
+    tg.send(msg.from.print_name, str)
+  },
+  '<date>': function (msg) {
+    var str = 'NOT CURRENTLY IMPLEMENTED. '
+    tg.send(msg.from.print_name, str)
+  }}
 
-/**********************************************************************
-			Listeners
+/* *********************************************************************
+      Listeners
 */
 
 tg.event.on('started', function (d) {
-	console.log('yo, this dawg is kicking')
+  console.log('yo, this dawg is kicking')
 })
 
 tg.event.on('newmessage', function (m) {
-	console.log('-------------------------------------'.magenta)
-	console.log(('we got a message from '+m.from.first_name).cyan)
+  console.log('-------------------------------------'.magenta)
+  console.log(('we got a message from ' + m.from.first_name).cyan)
 
-	// Check if a new user here TODO
-	// Check user id against those in db and then send welcome message if not there
-	// Then return early
+  // Check if a new user here TODO
+  // Check user id against those in db and then send welcome message if not there
+  // Then return early
 
-	// Check for keywords&commands
-	var words = m.text.replace('.',' ').split(" "),
-			caughtCommands = []
+  // Check for keywords&commands
+  var words = m.text.replace('.', ' ').split(' ')
+  var caughtCommands = []
 
-	words.forEach(function(word) {
-		if (word.toLowerCase() in keywords) caughtCommands.push(word.toLowerCase());
-	})
-	console.log('The following commands were caught:\n'+caughtCommands)
+  words.forEach(function (word) {
+    if (word.toLowerCase() in keywords) caughtCommands.push(word.toLowerCase())
+  })
+  console.log('The following commands were caught:\n' + caughtCommands)
 
+  // There was nothing so this must be a diary entry
+  if (caughtCommands.length === 0) { // No commands
+    addToDatabase(m.from.id, m.text, +new Date)
 
-	// There was nothing so this must be a diary entry
-	if (caughtCommands.length==0) { // No commands
-		addToDatabase(m.from.id, m.text, +new Date);
+    // Strip the white space. It'll kill the message
+    m.text = m.text.replace(/\r?\n|\r/g, ' ')
+    tg.send(m.from.print_name, 'A new diary entry was added saying: "' + m.text + '"')
 
-		// Strip the white space. It'll kill the message
-		m.text = m.text.replace(/\r?\n|\r/g, ' ')
-		tg.send(m.from.print_name, 'A new diary entry was added saying: "'+m.text+'"')
+  // We caight a specific help command
+  } else if (caughtCommands.length === 2 && caughtCommands.indexOf('<help>') >= 0) {
+    // Find out what the other command is and send it's help message back.
+    var helpPos = caughtCommands.indexOf('<help>')
+    var otherCommandPos = (helpPos === 0) ? 1 : 0
+    console.log('returning help message for ' + caughtCommands[otherCommandPos])
+    tg.send(m.from.print_name, statics.helpStrings[caughtCommands[otherCommandPos]])
 
-	// We caught a specific help command
-	} else if(caughtCommands.length==2 && caughtCommands.indexOf('<help>')>=0) {
-		// Find out what the other command is and send it's help message back.
-		helpPos = caughtCommands.indexOf('<help>')
-		otherCommandPos = (helpPos==0)? 1:0
-		console.log('returning help message for '+caughtCommands[otherCommandPos])
-		tg.send(m.from.print_name, statics.helpStrings[caughtCommands[otherCommandPos]])
+  // We caught a single command and are running it
+  } else if (caughtCommands.length === 1) { // run the first command
+    // run the commands function
+    keywords[caughtCommands[0]](m)
 
-	// We caught a single command and are running it
-	} else if(caughtCommands.length==1){ // run the first command
-		// run the commands function
-		keywords[caughtCommands[0]](m)
-
-	// Incase they went nuts with the commands nothing happens and they get a note.
-	} else {
-		tg.send(m.from.print_name, 'Too many commands were detected. ' +
-		'Please only send 1, or 2 when using the <help> command')
-	}
+  // Incase they went nuts with the commands nothing happens and they get a note.
+  } else {
+    tg.send(m.from.print_name, 'Too many commands were detected. ' +
+    'Please only send 1, or 2 when using the <help> command')
+  }
 
 })
 
 tg.event.on('longMessage', function (d) {
-	console.log('We got a long message:'.cyan)
-	console.log(d)
+  console.log('We got a long message:'.cyan)
+  console.log(d)
 })
 
 tg.event.on('unknownString', function (d) {
-	console.log('Caught an unknown string'.red)
-	console.log(d)
+  console.log('Caught an unknown string'.red)
+  console.log(d)
 })
 
 tg.event.on('sentmessage', function (d) {
-	console.log('A message was sent'.cyan)
+  console.log('A message was sent'.cyan)
 })
 
 tg.event.on('other', function (d) {
-	console.log('Someother type of message was caught'.cyan);
-	console.log(d)
+  console.log('Someother type of message was caught'.cyan)
+  console.log(d)
 })
 
 tg.event.on('readReceipt', function (d) {
-	console.log('User '.cyan+d.toString().cyan+' just triggered a read receipt'.cyan)
+  console.log('User '.cyan + d.toString().cyan + ' just triggered a read receipt'.cyan)
 })
 
-process.stdin.on('readable', function() {
-	var chunk = process.stdin.read()
-	if (chunk !== null) {
-		process.stdout.write('sending: "'+chunk.toString().trim()+'"\n')
-		tg.write(chunk.toString().trim())
-	}
+process.stdin.on('readable', function () {
+  var chunk = process.stdin.read()
+  if (chunk !== null) {
+    process.stdout.write('sending: "' + chunk.toString().trim() + '"\n')
+    tg.write(chunk.toString().trim())
+  }
 })
 
-process.on('exit', function(code) {
-	tg.close()
+process.on('exit', function (code) {
+  tg.close()
 })
 
-/**************************************************************************
-			Functions
+/* *************************************************************************
+      Functions
 */
 
-addToDatabase = function (id, text, date) {
-	// Pull information about user using id and user collection here
+var addToDatabase = function addToDatabase (id, text, date) {
+  // Pull information about user using id and user collection here
 
-	// Encrypt the user's information here
+  // Encrypt the user's information here
 
-	// Send the diary entry here
-	db.entries.insert({
-		text: text
-	, user: id
-	, date: date
-	})
+  // Send the diary entry here
+  db.entries.insert({
+    text: text,
+    user: id,
+    date: date
+  })
 }
--- tg.js
+++ tg.js
@@ -1,17 +1,17 @@
-var spawn 	 	 = require('child_process').spawn
-  , stripAns 	 = require('strip-ansi')
-  , EventEmitter = require('events').EventEmitter
-  , tgsh 		 = spawn('./tg.sh')
-  , colors 		 = require('colors')
+var spawn = require('child_process').spawn
+var stripAns = require('strip-ansi')
+var EventEmitter = require('events').EventEmitter
+var tgsh = spawn('./tg.sh')
+var colors = require('colors')
 
-function tg() {
+function tg () {
 
 }
 
 // A method
 tg.start = function () {
-	this.write('').write('dialog_list')
-	return this
+  this.write('').write('dialog_list')
+  return this
 }
 
 tg.sh = tgsh
@@ -25,38 +25,37 @@ tg.event = new EventEmitter()
 
 // This does actually send texts!
 tg.send = function (contact, str) {
-	tgsh.stdin.write('msg ' + contact + ' ' + str+'\n')
-	return this
+  tgsh.stdin.write('msg ' + contact + ' ' + str + '\n')
+  return this
 }
 
 tg.write = function (str) {
-	tgsh.stdin.write(str+'\n')
-	return this
+  tgsh.stdin.write(str + '\n')
+  return this
 }
 
-tg.addContact = function(phone, firstName, LastName) {
-	this.write('add_contact ')
+tg.addContact = function (phone, firstName, LastName) {
+  this.write('add_contact ')
   return this
 }
 
 tg.raw = []
 
-
 // Handle the output of tgcli
-tgsh.stdout.on('data', function(buffer) {
-	// console.log('wegotdataaswell'.cyan)
-	var lines = buffer.toString().split('\n').filter(Boolean)
+tgsh.stdout.on('data', function (buffer) {
+  // console.log('wegotdataaswell'.cyan)
+  var lines = buffer.toString().split('\n').filter(Boolean)
 
-	// Check if we just started up and handle the beautiful text.
-	// TODO: Pass this through to stdout to preserve cpright?
-	if (lines[0] == "Telegram-cli version 1.3.1, Copyright (C) 2013-2015 Vitaly Valtman") {
-		tg.event.emit('started')
+  // Check if we just started up and handle the beautiful text.
+  // TODO: Pass this through to stdout to preserve cpright?
+  if (lines[0] === 'Telegram-cli version 1.3.1, Copyright (C) 2013-2015 Vitaly Valtman') {
+    tg.event.emit('started')
     // console.log('wegothere'.green)
 
-  } else if (lines[0].split(" ")[0] == 'User') {
+  } else if (lines[0].split(' ')[0] === 'User') {
     // We just got told a user read a message. We don't /really/ car about this.
-    var tmp = lines[0].split(" ")
-    print_name = tmp[1]+'_'+tmp[2]
+    var tmp = lines[0].split(' ')
+    var print_name = tmp[1] + '_' + tmp[2]
 
     // We'll emit an event with the user's name.
     tg.event.emit('readReceipt', print_name)
@@ -65,20 +64,20 @@ tgsh.stdout.on('data', function(buffer) {
     tg.event.emit('longMessage', lines)
 
   // Every other time we do this:
-  }	else {
+  } else {
 
-		// Dev stuff.
-		// console.log('lines: '.red + lines.length.toString().red)
-		// tg.raw.push(lines)
-		// console.log(lines.toString().green)
+    // Dev stuff.
+    // console.log('lines: '.red + lines.length.toString().red)
+    // tg.raw.push(lines)
+    // console.log(lines.toString().green)
 
-		// Usually the important stuff is here
-		var msg = ''
+    // Usually the important stuff is here
+    var msg = ''
 
-		// If we just sent a message we'll get stars though, so catch that
-		if(lines[0].trim().split(' ')[0] == '***') {
-      var msg = undefined
-			lines.forEach(function (l) {
+    // If we just sent a message we'll get stars though, so catch that
+    if (lines[0].trim().split(' ')[0] === '***') {
+      msg = undefined
+      lines.forEach(function (l) {
         if (l.trim().split(' ')[0] !== '***') msg = l
       })
       // parse it
@@ -86,41 +85,38 @@ tgsh.stdout.on('data', function(buffer) {
         tg.event.emit('unknownString', lines)
         return
       }
-			msg = JSON.parse(msg)
+      msg = JSON.parse(msg)
 
-		} else {
+    } else {
       try {
         msg = JSON.parse(lines[0])
-      }
-      catch(e) {
+      } catch (e) {
         tg.event.emit('unknownString', lines)
         return
       }
-		}
+    }
 
-		// Was the msg about a message?
-		if ('text' in msg) {
+    // Was the msg about a message?
+    if ('text' in msg) {
 
-			// console.log("We received a ".red + msg.event.toString().red + ' event'.red)
+      // console.log("We received a ".red + msg.event.toString().red + ' event'.red)
 
-			// Was it to us?
-			if(msg.to.username == "sentencediary") {
-				// console.log('It was to us')
-				tg.event.emit('newmessage', msg)
-			}
+      // Was it to us?
+      if (msg.to.username === 'sentencediary') {
+        // console.log('It was to us')
+        tg.event.emit('newmessage', msg)
+      }
 
-			// Was it from us?
-			if(msg.from.username == "sentencediary") {
-				// console.log('It was from us')
-				tg.event.emit('sentmessage', msg)
-			}
+      // Was it from us?
+      if (msg.from.username === 'sentencediary') {
+        // console.log('It was from us')
+        tg.event.emit('sentmessage', msg)
+      }
 
-		} else {
+    } else {
       tg.event.emit('other', msg)
     }
-	}
+  }
 })
-
-
 
 exports = module.exports = tg

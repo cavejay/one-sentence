56b9991421d614be53f5435accd35961d45d32bf
Michael Ball <michael@sablecliff.com>
2016-02-02 14:56:50 +1000

    now properly fetches commits that match the tg 1.3.1 tag

--- setup.sh
+++ setup.sh
@@ -6,7 +6,8 @@
      echo "This is a MAC :D. Please have homebrew installed."
 brew install libconfig readline lua python libevent jansson
  export CFLAGS="-I/usr/local/include -I/usr/local/Cellar/readline/6.3.8/include"
- export LDFLAGS="-L/usr/local/lib -L/usr/local/Cellar/readline/6.3.8/lib"
+ export CPPFLAGS="-I/usr/local/opt/openssl/include"
+ export LDFLAGS="-L/usr/local/opt/openssl/lib -L/usr/local/lib -L/usr/local/Cellar/readline/6.3.8/lib"
 
  elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
      # Do something under Linux platform
@@ -17,7 +18,9 @@ sudo apt-get install libreadline-dev libconfig-dev libssl-dev lua5.2 liblua5.2-d
 git clone --recursive https://github.com/vysheng/tg.git
 cd tg
 git checkout tags/1.3.1
-./configure
+cd tgl && git checkout b3dcce35110f5c995366318c2886065287815d09
+cd tl-parser && git checkout ec8a8ed7a4f22428b83e21a9d3b5815f7a6f3bd9
+cd ../.. && ./configure
 make
 cd ..
 npm install .

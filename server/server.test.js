c80cadc2e811f5190a3c37486bbbb4d78a64d208
Michael Ball <michael@sablecliff.com>
2016-08-08 23:15:18 -0400

    added server helper function tests and updated the test script

--- server/server.test.js
+++ server/server.test.js
@@ -1,1 +1,10 @@
-// for tests. 
+// for tests.
+describe('helper functions', function () {
+  describe('make_session', function () {
+    it('correctly stores a sessionID and uid');
+  });
+
+  describe('getSessionExpiry', function () {
+    it('correctly decodes the session from a uid');
+  });
+});
